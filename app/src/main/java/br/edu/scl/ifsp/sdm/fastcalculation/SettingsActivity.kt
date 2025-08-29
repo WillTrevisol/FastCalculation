43d2d31a1fae625b9b454581b6e27b0fdc593e9c
Willian Trevisol <trevisolwillian@gmail.com>
2025-08-28 22:25:08 -0300

    feat: add activity_settings layout

--- app/src/main/java/br/edu/scl/ifsp/sdm/fastcalculation/SettingsActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/fastcalculation/SettingsActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.scl.ifsp.sdm.fastcalculation.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+    private val activitySettingsBinding: ActivitySettingsBinding by lazy {
+        ActivitySettingsBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_settings)
+        setContentView(activitySettingsBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
